29bbc4b996815b834787a406577ec7096c89e46c
mirkin99 <valentindorogan6@gmail.com>
2026-09-21 13:20:05 +0300

    Lily photo from Google Drive

--- lily-loader.js
+++ lily-loader.js
@@ -1,7 +1,10 @@
 (function(){
-  var b64 = (window.__LILY_A||"")+(window.__LILY_B||"");
-  if (!b64) return;
   var el = document.querySelector(".hero-lily-photo");
   if (!el) return;
-  el.style.backgroundImage = "url(data:image/jpeg;base64," + b64 + ")";
+  var url = "https://lh3.googleusercontent.com/d/11TmwNrmV8NEkrVwDq9aKsArErONse3tV=s1200";
+  var fb = "https://drive.google.com/thumbnail?id=11TmwNrmV8NEkrVwDq9aKsArErONse3tV&sz=w1200";
+  el.style.backgroundImage = "url(" + url + ")";
+  var test = new Image();
+  test.onerror = function(){ el.style.backgroundImage = "url(" + fb + ")"; };
+  test.src = url;
 })();
