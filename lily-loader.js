ab0eb81c8769c2b631df45240e0ec1aa9596bca0
mirkin99 <valentindorogan6@gmail.com>
2026-09-21 13:25:45 +0300

    Load hero lily from embedded base64 (new photo)

--- lily-loader.js
+++ lily-loader.js
@@ -1,10 +1,7 @@
 (function(){
+  var b64 = (window.__LILY_A||"")+(window.__LILY_B||"");
+  if (!b64) return;
   var el = document.querySelector(".hero-lily-photo");
   if (!el) return;
-  var url = "https://lh3.googleusercontent.com/d/11TmwNrmV8NEkrVwDq9aKsArErONse3tV=s1200";
-  var fb = "https://drive.google.com/thumbnail?id=11TmwNrmV8NEkrVwDq9aKsArErONse3tV&sz=w1200";
-  el.style.backgroundImage = "url(" + url + ")";
-  var test = new Image();
-  test.onerror = function(){ el.style.backgroundImage = "url(" + fb + ")"; };
-  test.src = url;
+  el.style.backgroundImage = "url(data:image/jpeg;base64," + b64 + ")";
 })();
